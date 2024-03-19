ac405fcf2e2736c1589135db36e8a4a0e09da880
Sagar Patil <sagar.22111052@viit.ac.in>
2024-03-19 13:22:57 +0530

    Rolling_Dice_Simulator.py

--- Rolling_Dice_Simulator.py
+++ Rolling_Dice_Simulator.py
@@ -1,9 +1,22 @@
 from tkinter import *
 import random
 
-root =Tk()
-root.title('Roll the dice')
-root.geometry("500x500")
+print("---------------------------------7,UP or DOWN---------------------------------")
+print("Rules of the game:")
+print("1.This is a betting game.")
+print("2.There are 3 slots where you can place  your money .")    
+print("Slot 1:7 DOWN")
+print("Slot 2:EXACT 7")
+print("Slot 3:7 UP")
+print("3.After placing your money on any of these slots ,2 dice are rolled.If the sum of the 2 numbers on the face of the dice satisfies the condtion on which you had")
+print("kept your money ,then you win and get respective awards,else you lose your money.")
+print()
+print("AWARDS:")
+print("a. 7 down: 2x your money")
+print("b. Exact 7: 4x your money")
+print("c. 7 up: 2x your money")  
+print() 
+
 
 def get_number(x):
     if x =='\u2680':
@@ -19,7 +32,24 @@ def get_number(x):
     elif x =='\u2685':
         return(6)
 
+name=input("Please enter your name:")
+slot=input("Please choose your slot:")
+money=int(input("Please set the amount of money you want to bet on your respective slot:₹"))  
+
+x=0
+
+root =Tk()
+root.title('Roll the dice')
+root.geometry("500x500")
+
 def roll_dice():
+ 
+    x=x+1
+    if x!=1:
+        root =Tk()
+        root.title('Roll the dice')
+        root.geometry("500x500")
+     
     d1 =random.choice(my_dice)
     d2 = random.choice(my_dice)
 
@@ -34,26 +64,7 @@ def roll_dice():
 
     total =sd1+sd2
     total_label.config(text =f"Sum : {total}")
-    
-    print("---------------------------------7,UP or DOWN---------------------------------")
-    print("Rules of the game:")
-    print("1.This is a betting game.")
-    print("2.There are 3 slots where you can place  your money .")    
-    print("Slot 1:7 DOWN")
-    print("Slot 2:EXACT 7")
-    print("Slot 3:7 UP")
-    print("3.After placing your money on any of these slots ,2 dice are rolled.If the sum of the 2 numbers on the face of the dice satisfies the condtion on which you had")
-    print("kept your money ,then you win and get respective awards,else you lose your money.")
-    print()
-    print("AWARDS:")
-    print("a. 7 down: 2x your money")
-    print("b. Exact 7: 4x your money")
-    print("c. 7 up: 2x your money")  
-    print() 
-    
-    name=input("Please enter your name:")
-    slot=input("Please choose your slot:")
-    money=int(input("Please set the amount of money you want to bet on your respective slot:₹"))  
+
     if total<7 and slot.upper().strip()=="7 DOWN":
         
         print(name," YOU WIN!")
